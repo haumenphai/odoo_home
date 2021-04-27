79223d3f216ae1a662fe65223830d0174c8b5468
haumenphai <codegunpow@gmail.com>
2021-04-27 19:49:12 +0700

    complete exam3 and exmam4

---
+++ custom_addons/test.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+
+
+# <span class="copy" data-original-title=""
+# : 3002226135, 3002226336
+
+
+
+# soup = BeautifulSoup(response.content, 'html.parser')
+# print(soup)
+
+
+# def _get_url_tax(tax):
+
+    # return url
+
+
+
+
+# print(_crawl_data('3002226135'))
+# url = _get_url_tax('3002226135')
+# print(url)
+# print(_crawl_data('https://masothue.com/3002226135-cong-ty-tnhh-mtv-xay-dung-duc-phat'))
+
+# print(_crawl_data('https://masothue.com/3002226135-cong-ty-tnhh-mtv-xay-dung-duc-phat'))
+# print(_crawl_data('https://masothue.com/3002226135-cong-ty-tnhh-mtv-xay-dung-duc-phat'))
