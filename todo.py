82c183ecfd0015ed715e3a2619b6fc814d5b6c98
haumenphai <codegunpow@gmail.com>
2021-05-19 10:26:47 +0700

    add exam

--- todo.py
+++ todo.py
@@ -1,7 +1,7 @@
 # git config --global credential.helper store
 # rsync -avHe ssh user@server:/path/to/file /home/user/path/to/file
 # --geoip-db=~/home/do/Desktop/file/GeoLite2-City_20210511/GeoLite2-City.mmdb
-
+# pkill -f odoo
 #
 # sudo systemctl start postgresql
 # sudo systemctl status postgresql
@@ -17,3 +17,15 @@ to_postgresql_server
 to_server
 """
 #-------------------------------------------------------------
+"""
+-u
+to_server_shh
+-u
+to_dns
+-u
+to_nginx_server
+-u
+to_postgresql_server
+-u
+to_server
+"""
