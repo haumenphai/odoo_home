94fff10fe598f253ab7eb0f8f37bb354a8934fa5
haumenphai <codegunpow@gmail.com>
2021-05-19 23:30:29 +0700

    ad

--- addons-bt/viin_exam_12/models/account_move.py
+++ addons-bt/viin_exam_12/models/account_move.py
@@ -5,34 +5,39 @@ from odoo import models, fields
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-    merge_invoice_line_ids = fields.One2many('account.move.line', 'move_id', string='Merge invoice lines',
-                                             readonly=True)
+    merge_invoice_line_ids = fields.One2many('account.move.line', 'move_id', string='Merge invoice lines')
 
     def merge(self):
         self.ensure_one()
         invoice_length = len(self.invoice_line_ids)
-        print(self.invoice_line_ids)
+        print('self.invoice_line_ids', self.invoice_line_ids)
 
-        invoice_lines = list(self.invoice_line_ids)
-        print(invoice_lines)
-
-        list1 = []
-        for r in self.invoice_line_ids:
-            list1.append(r.read(['name']))
-        print(list1)
+        invoice_lines = self.invoice_line_ids
 
         for i in range(0, invoice_length - 1):
             for j in range(i + 1, invoice_length):
                 if (invoice_lines[i].product_id == invoice_lines[j].product_id
                         and invoice_lines[i].price_unit == invoice_lines[j].price_unit
                         and invoice_lines[i].tax_ids == invoice_lines[j].tax_ids):
-                    print('a')
-                    # invoice_lines[i].quantity += invoice_lines[j].quantity
-                    # invoice_lines[i].price_unit += invoice_lines[j].price_unit
-                    # del invoice_lines[j]
-                    # invoice_length -= 1
+                    print('aaaaaaa')
 
-        print(invoice_lines)
+                    self.write({
+                        'merge_invoice_line_ids': [
+                            (1, invoice_lines[i].id, {
+                            'price_unit': invoice_lines[i].price_unit + invoice_lines[j].price_unit,
+                            'quantity': invoice_lines[i].quantity + invoice_lines[j].quantity
+                            }),
+                            (2, invoice_lines[j].id, 0)
+                        ]
+                    })
+                    invoice_length -= 1
 
-        for r in invoice_lines:
-            print('ten:', r.product_id.name, 'sl:', r.quantity, 'tax:', r.tax_ids.name)
+        print('a1', self.invoice_line_ids)
+        print(self.merge_invoice_line_ids)
+        # print('a2', self.merge_invoice_line_ids)
+
+
+        # print(self.merge_invoice_line_ids[0].price_unit)
+        # print(self.invoice_line_ids[0].price_unit)
+        # self.invoice_line_ids = invoice_lines
+
